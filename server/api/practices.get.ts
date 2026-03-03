4ce640002eb9618da3299a0ec48a6f67a5d69f32
Jukka Raimovaara <roska@mentalhouse.fi>
2026-03-03 14:18:56 +0200

    maybe fix the practices

--- server/api/practices.get.ts
+++ server/api/practices.get.ts
@@ -4,6 +4,7 @@ import { getPracticesData } from "~/utils/practices"
 export default defineEventHandler(async (event) => {
   const { cloudflare } = event.context
   const practices = await cloudflare.env.KV.get('practices', 'json')
+  console.log('practices data from KV:', practices)
   if (practices) {
     if (Array.isArray(practices.data) && practices.data.length > 0 && Date.now() - practices.fetched < 24 * 60 * 60 * 1000) {
       return practices.data
@@ -12,13 +13,12 @@ export default defineEventHandler(async (event) => {
     }
   }
   try {
-    getPracticesData().then(async (practices) => {
-      await cloudflare.env.KV.put('practices', JSON.stringify({ data: practices, fetched: Date.now() }), { expirationTtl: 24 * 60 * 60 })
-      return practices
-    })
+    const data = await getPracticesData()
+    await cloudflare.env.KV.put('practices', JSON.stringify({ data, fetched: Date.now() }), { expirationTtl: 24 * 60 * 60 })
+    return data
   }
   catch (e) {
     console.error('Error getting practices data', e)
   }
-  return [] 
+  return []
 })
